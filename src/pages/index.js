ca903cf146fc37aecff4f7dd7fbe648e7b3ebce5
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-19 11:20:55 -0500

    render posts on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,40 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import './post.css';
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+const IndexPage = (props) => {
+  const postList = props.data.allMarkdownRemark;
+  return (
+    <Layout>
+      {postList.edges.map(({ node }, i) => (
+        <Link to={node.fields.slug} className="link" >
+          <div className="post-list">
+            <h1>{node.frontmatter.title}</h1>
+            <span>{node.frontmatter.date}</span>
+            <p>{node.excerpt}</p>
+          </div>
+        </Link>
+      ))}
+    </Layout>
+  )
+}
+export default IndexPage;
+export const listQuery = graphql`
+  query ListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          fields{
+            slug
+          }
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM Do YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
