2857d07ec4bae83927222a4a8f7761932d29c7f7
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-19 11:11:34 -0500

    created blog post template

---
+++ src/templates/blog-post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+function BlogPost() {
+    return (
+        <Layout>
+            <div>
+                hello post
+        </div>
+        </Layout>
+    )
+}
+export default BlogPost
+
