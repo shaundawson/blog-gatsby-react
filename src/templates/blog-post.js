6b382cdfbaca94dd19eddba3f6887a66d509b753
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-19 11:13:07 -0500

    added query to blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql } from 'gatsby'
 function BlogPost() {
     return (
         <Layout>
@@ -10,4 +11,12 @@ function BlogPost() {
     )
 }
 export default BlogPost
-
+const query = graphql`
+ query PostQuery($slug: String!) {
+     markdownRemark(fields: { slug: { eq: $slug } }) {
+       html
+       frontmatter {
+        title
+        description
+       }
+   }
