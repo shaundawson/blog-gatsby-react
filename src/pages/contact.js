a044cea92accc273b29d39c465b41009c64189c1
Shaun Dawson <shaundawson0883@gmail.com>
2019-02-02 14:43:02 -0800

    php contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';  
 import Layout from '../components/layout';
-import 'bulma/css/bulma.css';
 import './post.css';
+import axios from 'axios';
 
 
-const Contact = () => (
+const API_PATH = 'https://github.com/shaundawson/blog-gatsby-react/blob/master/public/inc/index.php';
+
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactName: '',
+      contactEmail: '',
+      contactMessage: '',
+      mailSent: false,
+      error: null
+    }
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    axios({
+      method: 'post',
+      url: `${API_PATH}`,
+      headers: { 'content-type': 'application/json' },
+      data: this.state
+    })
+      .then(result => {
+        this.setState({
+          mailSent: result.data.sent
+        })
+      })
+      .catch(error => this.setState({ error: error.message }));
+  };
+
+  render() {
+    return (
   <Layout>
-  <section>
+  <section id= "contact">
   <div className="contact contact-hero is-centered">
   <h2 className="title is-light"> Send us a message
   </h2>
+  <form action="#" id="contactForm" name="contactForm">
     <div className="field">
-      <label className="label is-light is-small">Name</label>
+      <label className="label is-light is-small" id="contactName">Name</label>
         <div className="control">
-        <input className="input is-dark is-small" type="text" placeholder="Your Name"/>
+        <input className="input is-dark is-small" type="text" size={35}  name="contactName" placeholder="Your Name"
+        value={this.state.contactName}
+        onChange={e => this.setState({ contactName: e.target.value })}
+        />
+        
         </div>
     </div>
-    <form accept-charset="UTF-8" action="https://formkeep.com/f/4af43b11e3a2" method="POST" name="utf8">
         <div className="field">
         <label className="label is-light is-small">Email</label>
   <div className="control has-icons-left has-icons-right">
-    <input className="input is-dark is-small" type="email" name="email" placeholder="Your Email" />
+    <input className="input is-dark is-small" type="email" size={35} name="contactEmail" id="contactEmail" placeholder="Your Email" 
+    value={this.state.contactEmail}
+    onChange={e => this.setState({ contactEmail: e.target.value })}
+    />
     <span className="icon is-small is-left">
       <i className="fas fa-envelope"></i>
     </span>
@@ -28,22 +66,37 @@ const Contact = () => (
 </div>
 
 <div className="field">
+<label className="label is-light is-small">Subject</label>
+  <div className="control">
+  <input className="input is-dark is-small" type="text" size={35} name="contactSubject" placeholder="Subject"/>
+  </div>
+</div>
+
+<div className="field">
   <label className="label is-light is-small">Message</label>
   <div className="control">
-    <textarea className="textarea is-dark is-small" name="message" placeholder="Type message here"></textarea>
+    <textarea className="textarea is-dark is-small" name="contactMessage" placeholder="Type message here" 
+    onChange={e => this.setState({ contactMessage: e.target.value })}
+    å
+    />
   </div>
 </div>
 <br></br>
 
 <div className="field is-grouped">
-  <div className="control">
-    <button className="button is-warning is-medium" type="submit">Send</button>
+    <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
+    <div className="control">
   </div>
+</div>
+<div>
+  {this.state.mailSent &&
+    <div>Thank you for contcting us.</div>
+  }
 </div>
 </form>
 </div>
 </section>
 </Layout>
-)
-
-export default Contact
+);
+}
+}
